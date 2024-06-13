f654175e2790692751c3a72bc7be4b8ece8954cb
Maykon Franchini <maykonfranchini2017@gmail.com>
2024-06-13 05:50:49 +0100

    db version + open connections + max connections queries + tests

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,11 +1,27 @@
 import database from "infra/database.js";
 
 export default async function status(request, response) {
-  const result = await database.query("SELECT 1 + 1 as sum;");
-
-  console.log(result.rows);
+  const databaseName = process.env.POSTGRES_DB;
+  const dbVersion = await database.query('SELECT version();')
+  const dbMaxConnections = await database.query('SHOW max_connections;')
+  const dbOpenedConnections = await database.query({
+    text: "SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1;",
+    values: [databaseName]
+  })
+//   const dbtest = await database.query('SELECT sum(numbackends) FROM pg_stat_database;')
+// console.log(dbtest);
+  
+  const updatedAt = new Date().toISOString()
+  const dbVersionFormated = dbVersion.rows[0].version.split(' ')[1]
+  const maxConnectionsFormated = Number(dbMaxConnections.rows[0].max_connections)
 
   response.status(200).json({
-    status: "Everithing looks good",
+    updated_at: updatedAt,
+    database: {
+      version: dbVersionFormated,
+      max_connections: maxConnectionsFormated,
+      opened_connections: dbOpenedConnections.rowCount,
+    }
+    
   });
 }
--- tests/integration/api/v1/status/get.spec.js
+++ tests/integration/api/v1/status/get.spec.js
@@ -1,4 +1,14 @@
 test("Get to api/v1/status should return 200", async () => {
   const response = await fetch("http://localhost:3000/api/v1/status");
+  const responseBody = await response.json()
+  const actualDate = new Date(responseBody.updated_at).toISOString()
+  console.log();
   expect(response.status).toBe(200);
+  expect(responseBody.updated_at).toBe(actualDate)
+  expect(responseBody.database.version).toBe('14.12')
+  expect(responseBody.database.max_connections).toBe(100)
+  expect(responseBody.database.opened_connections).toBe(1)
+  
+
+
 });
